d4da5ad27c4b435fe733b1fdf508aa5694d2d967
Sergey Gorohov <gamehouse02@yandex.ru>
2024-02-21 23:46:03 +0300

    Updated crossover function and fixed dimension bugs

--- genetic.py
+++ genetic.py
@@ -22,56 +22,69 @@ def initiation():
 def selection(generation):
     scores = np.ones(generation_size)
     for i in range(generation_size):
-        scores[i] = fpckg.perform_trial(generation[i], 3, 1)
+        scores[i] = fpckg.perform_trial(generation[i], 3, 3)
 
     return scores
 
 
 def random_specimen():
-    return np.random.randint(0, generation_size - 1, 1)
+    return np.random.randint(0, generation_size, 1)[0]
 
 
 def crossover(parent1, parent2):
-    return (parent1 + parent2) / 2
+    k = 20
+
+    child = parent1
+    for l in range(k):
+        i = np.random.randint(0, N, 1)[0]
+        j = np.random.randint(0, N, 1)[0]
+        child[i][j] = parent2[i][j]
+
+    return child
 
 
 def mutation(specimen):
     return specimen + np.random.randn(N, N) * 0.01
 
 
-def scores_plot(epochs, avg_scores, best_scores):
+def scores_plot(epochs, avg_scores, best_scores, glbl_scores):
     epochs_arr = np.arange(epochs)
     plt.plot(epochs_arr, avg_scores, label='Средний score (по поколению)')
     plt.plot(epochs_arr, best_scores, label='Лучший score (в поколении)')
+    plt.plot(epochs_arr, glbl_scores, label='score при gamma=0.5')
     plt.ylabel("Score")
     plt.xlabel("Эпоха")
     plt.legend()
     plt.show()
 
 
-def life_cycle(eps=3, epochs=30, mutation_prob=0.05):
+def life_cycle(eps=3, epochs=50, mutation_prob=0.08):
     generation = initiation()
     new_generation = []
 
     avg_scores = np.zeros(epochs)
     best_scores = np.zeros(epochs)
+    glbl_gamma_scores = np.zeros(epochs)
 
     for i in range(epochs):
         print("Epoch " + str(i))
         scores = selection(generation)
 
+        glbl_gamma_scores[i] = fpckg.perform_trial(np.ones((N, N)) * 0.5, 3, 2)
+
         avg_scores[i] = np.average(scores)
         best_scores[i] = np.min(scores)
 
-        best5 = np.argpartition(scores, -5)[-5:]
+        best5 = np.argpartition(scores, 5)[:5]
         print("5 best specimen are " + str(best5) + " with scores: " + str(scores[best5]))
+        print("Score with global gamma = 0.5: " + str(glbl_gamma_scores[i]))
 
         new_generation = generation
 
         for j in range(generation_size):
             if j not in best5:
                 new_generation[j] = crossover(generation[random_specimen()], generation[random_specimen()])
-            elif np.random.random() < mutation_prob:
+            if np.random.random() < mutation_prob:
                 new_generation[j] = mutation(generation[j])
                 print("Mutation occured on " + str(j) + "th specimen")
 
@@ -80,7 +93,7 @@ def life_cycle(eps=3, epochs=30, mutation_prob=0.05):
         print("\n")
 
     print("Life Cycle ended")
-    scores_plot(epochs, avg_scores, best_scores)
+    scores_plot(epochs, avg_scores, best_scores, glbl_gamma_scores)
 
 
 life_cycle()
