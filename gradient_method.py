0af97fc6cccd1b50db9c9ad5d7b3ca5a9f29c079
Sergey Gorohov <gamehouse02@yandex.ru>
2024-03-13 01:12:44 +0300

    Grad package updated

--- gradient_method.py
+++ gradient_method.py
@@ -2,22 +2,29 @@ import numpy as np
 from scipy.fft import fft2, ifft2, fftshift, ifftshift
 from method import dev_ss, dev_gamma, method_v
 import functions_package as fpckg
+from metrics import count_metrics
 
 
-def dev_mse_gammas(gammas, ss, u_orig, u_res, pnt_cnt, edge):
+def dev_mse_gammas(gammas, ss, u_orig, pnt_cnt, edge):
+    u_res = method_v(u_orig, pnt_cnt, edge, 1, gammas, ss)
     mse_dev = 2 * (u_orig - u_res)
-    return mse_dev * dev_gamma(gammas, ss, u_orig, pnt_cnt, edge)
+    dg = dev_gamma(gammas, ss, u_orig, pnt_cnt, edge)
+    return mse_dev * dg
 
 
-def dev_mse_ss(gammas, ss, u_orig, u_res, pnt_cnt, edge):
+def dev_mse_ss(gammas, ss, u_orig, pnt_cnt, edge):
+    u_res = method_v(u_orig, pnt_cnt, edge, 1, gammas, ss)
     mse_dev = 2 * (u_orig - u_res)
     return mse_dev * dev_ss(gammas, ss, u_orig, pnt_cnt, edge)
 
 
-def sopr_grad(grad_f, x0, eps=1e-8, max_iter=1000):
+def grad_descent(grad_f, x0, scnd_param, u_orig, pnt_cnt, edge, eps=1e-8, max_iter=10):
     # Initialzaing start values
     x = x0.copy()
-    g = grad_f(x)
+    # g = grad_f(x)
+    # g = dev_mse_gammas(x, scnd_param, u_orig, pnt_cnt, edge)
+    g = dev_mse_ss(scnd_param, x, u_orig, pnt_cnt, edge)
+    # print(g)
     d = -g
     delta_new = np.dot(g, g)
 
@@ -26,11 +33,13 @@ def sopr_grad(grad_f, x0, eps=1e-8, max_iter=1000):
         step_size = 0.001
 
         x = x + step_size * d
-        g = grad_f(x)
+        # g = grad_f(x)
+        # g = dev_mse_gammas(x, scnd_param, u_orig, pnt_cnt, edge)
+        g = dev_mse_ss(scnd_param, x, u_orig, pnt_cnt, edge)
         delta_old = delta_new
         delta_new = np.dot(g, g)
 
-        if np.sqrt(delta_new) < eps:
+        if np.sqrt(np.sum(delta_new)) < eps:
             break
 
         beta = delta_new / delta_old
@@ -48,3 +57,13 @@ y = np.linspace(-Edge, Edge, N, endpoint=False)
 Y, X = np.meshgrid(x, y)
 
 u_orig = fpckg.multifocal([1, 3], [0.8, -1.5], X, Y)
+
+gamma0 = np.ones((N, N)) * 0.5
+
+# fpckg.visualaize_param_matrix(gamma0)
+
+res = grad_descent(np.sin, np.ones((N, N)) * 0.5, 0.5, u_orig, N, Edge)
+
+fpckg.visualaize_param_matrix(res)
+
+# count_metrics(res)
