56af129267830b241d6cd56d4d6925d5292b9f2f
Sergey Gorohov <gamehouse02@yandex.ru>
2024-03-16 17:04:58 +0300

    Change graphs

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ y = np.linspace(-Edge, Edge, N, endpoint=False)
 Y, X = np.meshgrid(x, y)
 
 
-# z = multifocal([1, 3, 12], [0.8, -1.5, -11])
+# z = fpckg.multifocal([1, 3, 12], [0.8, -1.5, -11], X, Y)
 z = fpckg.multifocal([1, 3], [0.8, -1.5], X, Y)
 # z = multifocal_razr([np.sqrt(3), 3], [0.8, -1.5], [0, 1, 3])
 # z = fpckg.spiral(3, 1, X, Y)
@@ -22,13 +22,13 @@ z = fpckg.multifocal([1, 3], [0.8, -1.5], X, Y)
 
 # z = add_noise(z, 0.01, N)
 
-z_approx = method_v(z, N, np.pi, 1, gamma=0.5)
+z_approx = method_v(z, N, Edge, 1)
 
 offs = fpckg.offset(z_approx, z, N, fpckg.spiral_flag)
 
 
 fig1, ax = plt.subplots(subplot_kw={"projection": "3d"})
-surf2 = ax.plot_surface(X, Y, fpckg.prepare_for_visual(X, Y, z), cmap='plasma')
+surf2 = ax.plot_surface(X, Y, fpckg.prepare_for_visual(X, Y, z_approx - offs), cmap='plasma')
 ax.set_title('Spiral')
 ax.view_init(45, 60)
 plt.show()
