9fef0d07b8bd1d04f70bb9782e51d3489a9beb5c
Sergey Gorohov <gamehouse02@yandex.ru>
2024-03-16 17:05:22 +0300

    Remade gradient descent with torch

--- gradient_method.py
+++ gradient_method.py
@@ -1,56 +1,27 @@
 import numpy as np
-from scipy.fft import fft2, ifft2, fftshift, ifftshift
-from method import dev_ss, dev_gamma, method_v
+from method import method_v
 import functions_package as fpckg
 from metrics import count_metrics
-
-
-def dev_mse_gammas(gammas, ss, u_orig, pnt_cnt, edge):
-    u_res = method_v(u_orig, pnt_cnt, edge, 1, gammas, ss)
-    mse_dev = 2 * (u_orig - u_res)
-    dg = dev_gamma(gammas, ss, u_orig, pnt_cnt, edge)
-    return mse_dev * dg
-
-
-def dev_mse_ss(gammas, ss, u_orig, pnt_cnt, edge):
-    u_res = method_v(u_orig, pnt_cnt, edge, 1, gammas, ss)
-    mse_dev = 2 * (u_orig - u_res)
-    return mse_dev * dev_ss(gammas, ss, u_orig, pnt_cnt, edge)
-
-
-def grad_descent(grad_f, x0, scnd_param, u_orig, pnt_cnt, edge, eps=1e-8, max_iter=10):
-    # Initialzaing start values
-    x = x0.copy()
-    # g = grad_f(x)
-    # g = dev_mse_gammas(x, scnd_param, u_orig, pnt_cnt, edge)
-    g = dev_mse_ss(scnd_param, x, u_orig, pnt_cnt, edge)
-    # print(g)
-    d = -g
-    delta_new = np.dot(g, g)
-
-    for i in range(max_iter):
-        # step_size = delta_new / np.dot(d, grad_f(x))
-        step_size = 0.001
-
-        x = x + step_size * d
-        # g = grad_f(x)
-        # g = dev_mse_gammas(x, scnd_param, u_orig, pnt_cnt, edge)
-        g = dev_mse_ss(scnd_param, x, u_orig, pnt_cnt, edge)
-        delta_old = delta_new
-        delta_new = np.dot(g, g)
-
-        if np.sqrt(np.sum(delta_new)) < eps:
-            break
-
-        beta = delta_new / delta_old
-        d = -g + beta * d
-
-    return x
-
+import torch
+import matplotlib.pyplot as plt
 
 N = fpckg.N
 Edge = fpckg.Edge
 
+
+# Попробовать взять реализованный градиентный метод, рассчитанный на большую размерность задачи
+
+def criterion(output, target):
+    return torch.mean(torch.abs(output - torch.from_numpy(target)) ** 2)
+
+
+def forward(u_org, pnt_cnt, edge, gammas, ss=0.5):
+    u_res = method_v(u_org, pnt_cnt, edge, 1, gammas, ss, 1)
+    return u_res
+
+
+step_size = 10
+n_iter = 300
 
 x = np.linspace(-Edge, Edge, N, endpoint=False)
 y = np.linspace(-Edge, Edge, N, endpoint=False)
@@ -58,12 +29,56 @@ Y, X = np.meshgrid(x, y)
 
 u_orig = fpckg.multifocal([1, 3], [0.8, -1.5], X, Y)
 
-gamma0 = np.ones((N, N)) * 0.5
 
-# fpckg.visualaize_param_matrix(gamma0)
+class Params:
+    def __init__(self):
+        self.gamma = torch.tensor(torch.randn((N, N)), requires_grad=True)
 
-res = grad_descent(np.sin, np.ones((N, N)) * 0.5, 0.5, u_orig, N, Edge)
+    def __iter__(self):
+        return iter([self.gamma])
 
-fpckg.visualaize_param_matrix(res)
+
+p = Params()
+
+count_metrics(p.gamma.detach().numpy())
+
+
+epochs = torch.arange(n_iter)
+losses = torch.zeros(n_iter)
+optimizer = torch.optim.Adam(p, lr=0.1)
+
+for i in range(n_iter):
+    optimizer.zero_grad()
+    # making predictions with forward pass
+    Y_pred = forward(u_orig, N, Edge, p.gamma)
+
+    # calculating the loss between original and predicted data points
+    loss = criterion(Y_pred, u_orig)
+
+    # backward pass for computing the gradients of the loss w.r.t to learnable parameters
+    loss.backward()
+    optimizer.step()
+
+    # updateing the parameters after each iteration
+    #p.gamma.data = p.gamma.data - step_size * p.gamma.grad.data
+
+    # zeroing gradients after each iteration
+    p.gamma.grad.data.zero_()
+
+    losses[i] = loss.item()
+
+    # priting the values for understanding
+    if i % 20 == 0:
+        print('{}. \t{}'.format(i, loss.item()))
+
+count_metrics(p.gamma.detach().numpy())
+
+plt.plot(epochs, losses, label='Loss')
+plt.ylabel("MSE")
+plt.xlabel("Epoch")
+plt.legend()
+plt.show()
+
+# fpckg.visualaize_param_matrix(res)
 
 # count_metrics(res)
